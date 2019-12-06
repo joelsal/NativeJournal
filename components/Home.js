506b6b85acc260482259984d3350ec6d8cdaff18
joelsal <salonenjoppe@gmail.com>
2019-12-07 01:00:26 +0200

    moved journal onto home page, added ability to add images, automatically includes time and date

--- components/Home.js
+++ components/Home.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, FlatList, Image } from 'react-native';
+//import { ListItem } from 'react-native-elements';
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabase('test.db');
 
 export default function Home(props) {
     navigationOptions = {
@@ -7,15 +11,63 @@ export default function Home(props) {
     };
 
     const {navigate} = props.navigation;
+    const [data, setData] = useState([]);
+
+    const journalRefresh = props.navigation.addListener('willFocus', () => {
+      updateList();
+    });
+
+    useEffect(() => {
+      db.transaction(tx => {
+        tx.executeSql('create table if not exists test (id integer primary key not null, title text, text text, date text, image text);');
+      });
+      return() => {
+        journalRefresh.remove();
+      }
+    }, []);
+
+    const updateList = () => {
+      db.transaction(tx => {
+        tx.executeSql('select * from test;', [], (_, { rows }) =>
+          setData(rows._array)
+        );
+      });
+    }
+
+    const listSeparator = () => {
+      return (
+        <View
+          style={{
+            height: 5,
+            width: "80%",
+            backgroundColor: "#fff",
+            marginLeft: "10%"
+          }}
+        />
+      )
+    }
 
   return (
     <View style={styles.container}>
       <View style={{flex: 1}}>
         <Text>Welcome back!</Text>
       </View>
-      <View style={{flex: 3, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around'}}>
+      <View style={{flex: 1}}>
         <Button title="New Entry" onPress={() => navigate('NewEntry')} />
-        <Button title="Old Entries" />
+      </View>
+      <View style={{flex: 8, width: "95%"}}>
+        <Text style={{fontSize: 20}}>JOURNAL</Text>
+        <FlatList 
+          keyExtractor={item => item.id.toString()} 
+          renderItem={({item}) => <View style={styles.listcontainer}>
+                                    <Text style={styles.listtitle}>{item.title}</Text>
+                                    <Text style={styles.listtext}>{item.text}</Text>
+                                    <Text style={styles.listdate}>{item.date}</Text>
+                                    <Image style={styles.listimage} source={{uri: item.image}} />
+                                  </View>}
+          data={data}
+          ItemSeparatorComponent={listSeparator}
+        />
       </View>
     </View>
   );
@@ -30,4 +82,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listcontainer: {
+    backgroundColor: '#ECA6FF',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  listtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingLeft: 3
+  },
+  listtext: {
+    fontSize: 18,
+    paddingLeft: 3
+  },
+  listdate: {
+    fontSize: 12,
+    fontStyle: 'italic',
+    paddingLeft: 3
+  },
+  listimage: {
+    width: 50,
+    height: 100,
+    margin: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#003067'
+  }
 });
--- components/NewEntry.js
+++ components/NewEntry.js
@@ -1,26 +1,105 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Button, TextInput } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Button, TextInput, Text, Modal, Image } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
+import moment from 'moment';
+
+const db = SQLite.openDatabase('test.db');
 
 export default function NewEntry(props) {
   const [title, setTitle] = useState('');
   const [text, setText] = useState('');
+  const [date, setDate] = useState(moment(new Date()).format("DD-MM-YYYY hh:mm a"));
+  const [cameraModal, setCameraModal] = useState(false);
+  const [image, setImage] = useState('');
+  const [permissionGranted, setPermissionGranted] = useState(null)
+
+  const camera = useRef(null);
 
   navigationOptions = {
     title: 'New Entry',
   };
 
+  useEffect(() => {
+    askCameraPermission();
+  }, []);
+
+  askCameraPermission = async () => {
+    let { status } = await Permissions.askAsync(Permissions.CAMERA);
+    setPermissionGranted( status == 'granted');
+  }
+
+  const snap = async () => {
+    if (camera) {
+      let photo = await camera.current.takePictureAsync({base64: false});
+      setImage(photo.uri);
+      //setCameraModal(!cameraModal);
+      //console.log(image)
+    }
+    //console.log(image)
+  };
+
+  const addEntry = () => {
+      db.transaction(tx => {
+        tx.executeSql('insert into test (title, text, date, image) values (?, ?, ?, ?);', [title, text, date, image]);
+      }, console.log("An error occured..."), updateList
+    )
+  }
+
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from test;', [], (_, { rows }) =>
+        console.log(rows._array)
+      );
+    });
+    navigate('Home')
+  }
+
+  const toggleCameraModal = (visible) => {
+    setCameraModal(visible);
+  }
+
+  const {navigate} = props.navigation;
 
   return (
     <View style={styles.container}>
       <View style={{flex: 1, paddingTop: 20}}>
-        <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}} onChangeText={(title) => setTitle(title)} value={title} />
+        <Text>TITLE</Text>
+        <TextInput style={styles.titletext} onChangeText={(title) => setTitle(title)} value={title} />
       </View>
       <View style={{flex: 4}}>
-        <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}} onChangeText={(text) => setText(text)} value={text} />
+        <Text>TEXT</Text>
+        <TextInput style={styles.texttext} multiline={true} numberOfLines={10} textAlignVertical={'top'} onChangeText={(text) => setText(text)} value={text} />
       </View>
       <View style={{flex: 1, alignItems: 'center'}}>
-          <Button title="Add Entry" />
+          <Button title="TAKE A PICTURE" onPress={() => toggleCameraModal(true)} />
       </View>
+      <View style={{flex: 1, alignItems: 'center'}}>
+          <Button title="Add Entry" onPress={() => addEntry()} />
+      </View>
+
+      <Modal visible={cameraModal} onRequestClose={() => setCameraModal(!cameraModal)}>
+        <View style={{flex: 1}}>
+        { permissionGranted ?
+          (
+          <View style={{flex: 1}}>
+            <View style={{flex: 5}}>
+              <Camera style={{flex: 1}} ref={camera}/>
+            </View>
+            <View style={{flex: 1, alignItems: 'center', paddingTop: 10}}>
+              <Button title="Take a picture!" onPress={snap}/>
+            </View>
+            <View style={{flex: 4, alignItems: 'stretch', backgroundColor: "#FFC7D5"}}>
+              <Image style={styles.previewimage} source={{uri: image}}/>
+            </View>
+          </View>
+          ) : (
+            <Text>NativeJournal has no access to camera!</Text>
+          )}
+        </View>
+      </Modal>
+
     </View>
   );
 }
@@ -32,4 +111,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titletext: {
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  texttext: {
+    width: 300, 
+    borderColor: 'gray',
+    borderWidth: 1
+  },
+  previewimage: {
+    width: "100%",
+    height: "100%",
+    paddingBottom: 20,
+  }
 });
